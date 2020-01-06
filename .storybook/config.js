2348ede79c556662de7574d605cdbbaf6d2f1929
zhangsa <sandy.zhang@tabcorp.com.au>
2020-01-06 19:05:45 +1100

    finish storybook add

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,12 @@
-import { configure } from '@storybook/react'
+import { configure, addParameters } from '@storybook/react'
+
+addParameters({
+  options: {
+    name: 'css loaders',
+    isFullscreen: false,
+    showPanel: false,
+    brandUrl: 'https://github.com/sandylib/react-pure-css-loaders'
+  }
+})
 
 configure(require.context('../src', true, /\.stories\.js$/), module)
